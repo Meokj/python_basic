0bf04c1f55e92b65a447d9c55278b72463f48a59
co2f2e <co2f2e@users.noreply.github.com>
2026-02-03 16:26:40 +0800

    Initial commit

---
+++ 第3章/13_字符串_转义字符.py
@@ -0,0 +1,22 @@
+# 使用 \' 输出'
+print('在Python中，可以使用\'包裹一个字符串')
+# 使用 \" 输出 "
+print('在Python中，可以使用\"包裹一个字符串')
+# 使用 \n 进行换行
+print('注册会员需要以下信息：\n姓名\n年龄\n手机号\n')
+# 使用 \\输出 \
+print('D:\\nice')
+# 使用 \b 删除前一个字符
+print('helloo\b')
+# 使用 \r 使光标回到本行开头，覆盖输出
+print('67%\r68%')
+# 使用 \t 表示水平制表符（让光标跳转到下一个制表位）
+print('1234123412341234')
+# 不管是什么环境去执行这个代码，一个制表位就是由4位组成的
+print('ab\tcd'.expandtabs(4))
+print('abc\td'.expandtabs(4))
+print('abcd\ta'.expandtabs(4))
+
+print('姓名\t\t性别\t\t年龄')
+print('张三\t\t男\t\t18')
+print('李四\t\t男\t\t19')
